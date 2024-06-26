5b2712b17819343dbc4ca2ca284e525da8d78973
chirag <chiragsinghal04@gmail.com>
2024-06-26 17:03:44 +0530

    -commit

---
+++ src/redux/reducers/notificationReducer.js
@@ -0,0 +1,43 @@
+import {createSlice} from '@reduxjs/toolkit'
+import { actions } from './todoReducer';
+
+const initialState={
+    message:""
+}
+
+const notificationSlice = createSlice({
+    name:'notification',
+    initialState,
+    reducers:{
+        reset:(state,action)=>{
+            state.message="";
+        }
+    },
+    // extraReducers:{
+    //     "todo/add":(state, action)=>{
+    //         console.log("todo/add in notificationReducer");
+    //       state.message="Todo is created";  
+    //     }
+    // }
+
+
+    // extraReducers:(builder)=>{
+    //     builder.addCase(actions.add,
+    //         (state, action)=>{       
+    //              state.message="Todo is created";  
+    //         }
+
+    //     );
+    // }
+
+    extraReducers:{
+        [actions.add]:(state,action)=>{
+            state.message="Todo is created";
+        }
+    }
+});
+
+export const notificationReducer = notificationSlice.reducer;
+export const resetNoti= notificationSlice.actions.reset;
+
+export const notificationSelector = (state)=>state.notificationReducer.message;
