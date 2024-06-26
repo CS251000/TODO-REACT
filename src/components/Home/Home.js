e8f6096bd7ede4a7ef5d212c3414b8302caa1d29
chirag <chiragsinghal04@gmail.com>
2024-06-26 17:08:49 +0530

    -home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,24 @@
+// src/components/Home.js
 
+import React from "react";
 import "./Home.css";
 
-function Home(){
-
-    return(
-        <div className="home-container">
-            <a href="todo" className="btn btn-warning link">
-                To Do App
-            </a>
-            <a href="notes" className="btn btn-warning link">
-                Note Keeper
-            </a>
+function Home() {
+  return (
+    <div className="home-container">
+      <div className="content">
+        <h1 className="title">Welcome to Your App</h1>
+        <div className="buttons">
+          <a href="todo" className="btn btn-warning link">
+            To Do App
+          </a>
+          <a href="notes" className="btn btn-warning link">
+            Note Keeper
+          </a>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Home;
